324871301154ec1f25abc1781db93951eaa944f3
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-04-09 01:43:23 +0300

    created a quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormComponent } from './form/form.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FormComponent
+    FormComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
