dd9abe3ad9d231ce704eac6bff543d4ed28fbdc9
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-04-10 02:04:34 +0300

    quote input

---
+++ src/app/QuotePost.ts
@@ -0,0 +1,4 @@
+export class QuotePost{
+    name: string;
+    isCompleted : boolean;
+}
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Toadd } from '../Toadd';
 
 
 @Component({
@@ -7,6 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  toadds: Toadd[]=[];
+  newToadd: string;
+
+  saveToadd(){
+    if(this.newToadd){
+      let toadd = new Toadd();
+      toadd.name = this.newToadd;
+      toadd.isCompleted = true;
+      this.toadds.push(toadd);
+      this.newToadd = "";
+    }else{
+      alert("please add quote")
+    }
+  }
+
+  remove(id:number){
+    this.toadds = this.toadds.filter((v,i)=>i !==id);
+  }
 
 
   constructor() { }
