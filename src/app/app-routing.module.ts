61dccd9ea2ce874c1d41bdc2d638fd0b9ed5e699
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-04-09 09:25:07 +0300

    created model class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
+import { QuoteComponent } from './quote/quote.component';
 
 const routes: Routes = [
   {path:"", redirectTo:"form", pathMatch:"full"},
-  {path:"form", component:FormComponent}
+  {path:"form", component:FormComponent},
+  {path:"quote", component:QuoteComponent}
 ];
 
 @NgModule({
